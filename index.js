a38898cb8970b254453eb17a68c147f432c9c2fa
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-12-08 09:38:50 -0600

    keyframes not working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const playBtn = document.getElementById("play-btn")
 const pauseBtn = document.getElementById("pause-btn")
 const stopBtn = document.getElementById("stop-btn")
+const bell = document.getElementById("bell");
 
 // Task:
 // - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
@@ -12,12 +13,15 @@ const stopBtn = document.getElementById("stop-btn")
 const audio = new Audio('bells.mp3')
 playBtn.addEventListener('click', ()=>{
     audio.play();
+    bell.classList.add("animate");
 })
 
 pauseBtn.addEventListener('click', ()=> {
     audio.pause();
+    bell.classList.remove("animate");  
 })
 
 stopBtn.addEventListener('click', ()=> {
     audio.load();
+    bell.classList.remove("animate"); 
 })
